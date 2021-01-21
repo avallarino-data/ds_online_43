128aeb6cf9d64bef2884a7ed097b74d79f2a6de8
AVallarino <avallarino@mymachine.local>
2021-01-21 12:27:12 -0600

    subo script con ejercicios

---
+++ code_and_git.py
@@ -0,0 +1,61 @@
+# # Ejercicio _colabirativo_
+
+import numpy as np
+
+
+# **Ejercicio 1**: Escribir un arreglo con 100 números equiespaciados del 0 al 9. Pista: `linspace`
+
+# COMPLETAR
+
+
+# **Ejercicio 2:** crear un arreglo 1D de 20 ceros. 
+# Reemplazar los primeros 15 elementos por unos.
+
+# COMPLETAR
+
+
+# **Ejercicio 3:** crear un arreglo 1D de 50 ceros. 
+# Reemplazar los primeros 25 elementos por los números naturales del 0 al 24.
+
+# COMPLETAR
+
+
+# **Ejercicio 4:** crear un arreglo 2D de 3 filas y 3 columnas, lleno de ceros. 
+# Reemplazar los elemento de la segunda columna por los números 1, 2 y 3 respectivamente. 
+# Es decir, crear la siguiente matriz:
+# 
+# ```
+# 0 1 0 
+# 0 2 0
+# 0 3 0
+# ```
+
+# COMPLETAR
+
+
+# **Ejercicio 5:** crear un arreglo 2D de 3 filas y 3 columnas, lleno de ceros. 
+# Reemplazar los elemento de la diagonal por unos. Es decir, crear la siguiente matriz:
+# 
+# ```
+# 1 0 0 
+# 0 1 0
+# 0 0 1
+# ```
+
+# COMPLETAR
+
+# **Ejercicio 6:** crear un arreglo 2D de 100 filas y 100 columnas, lleno de ceros. 
+# Reemplazar los elemento de la diagonal por unos. Es decir, crear la siguiente matriz:
+# 
+# ```
+# 1 0 0 ... 0 0 0 
+# 0 1 0 ... 0 0 0
+# 0 0 1 ... 0 0 0
+# ...  
+# 0 0 0 ... 1 0 0
+# 0 0 0 ... 0 1 0
+# 0 0 0 ... 0 0 1
+# ```
+
+# COMPLETAR
+
